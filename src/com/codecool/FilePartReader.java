2b07b1719da39518a9f3f2158e29b5f3e8b0005f
Michal Kalinowski <subaru.kali@gmail.com>
2019-06-13 11:43:22 +0200

    readLines method without parameters

--- src/com/codecool/FilePartReader.java
+++ src/com/codecool/FilePartReader.java
@@ -1,14 +1,17 @@
 package com.codecool;
 
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.List;
 
 public class FilePartReader {
 
+    public FilePartReader() {
+        String filePath = "invalid";
+        int fromLine = -1;
+        int toLine = -1;
+    }
 
     public void setup(String filePath, int fromLine, int toLine) {
 
@@ -17,14 +20,19 @@ public class FilePartReader {
         }
     }
 
-    public String read() throws IOException {
+    private String read() throws IOException {
         File file = new File("/home/kali/codecool/JAVA/filepartreader-testing-with-junit-KALIWWA/src/com/codecool/test.txt");
         StringBuilder content = new StringBuilder();
         List<String> allLinesList = Files.readAllLines(file.toPath());
-        for (int i = 0; i < allLinesList.size() - 1; i++) {
-            content.append(allLinesList.get(i));
-            content.append(",");
+        for (int i = 0; i < allLinesList.size(); i++) {
+            if (i < allLinesList.size() - 1) {
+                content.append(allLinesList.get(i));
+                content.append(",");
+            } else {
+                content.append(allLinesList.get(i));
+            }
         }
+        System.out.println(content + " <- read()");
         return String.valueOf(content);
 
     }
@@ -35,9 +43,9 @@ public class FilePartReader {
             String fileString = read();
             String[] allLinesList = fileString.split(",");
 
-            for (int i = 1; i <= toLine; i++) {
+            for (int i = 0; i <= toLine; i++) {
                 if (i >= fromLine) {
-                    String singleLine = allLinesList[i];
+                    String singleLine = allLinesList[i - 1];
                     requestedLines.append(singleLine);
                 }
             }
@@ -45,6 +53,24 @@ public class FilePartReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(requestedLines + " <- readLines(int fromLine, int toLine)");
+        return String.valueOf(requestedLines);
+    }
+
+    public String readLines() {
+        StringBuilder requestedLines = new StringBuilder();
+        try {
+            String fileString = read();
+            String[] allLinesList = fileString.split(",");
+
+            for (String singleLine : allLinesList) {
+                requestedLines.append(singleLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(requestedLines + " <- readLines()");
         return String.valueOf(requestedLines);
     }
 
---
+++ src/com/codecool/Main.java
@@ -0,0 +1,9 @@
+package com.codecool;
+
+public class Main {
+    public static void main(String[] args) {
+        FilePartReader filePartReader = new FilePartReader();
+        filePartReader.readLines(2,4);
+        filePartReader.readLines();
+        }
+}
